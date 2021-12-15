1897facd5c2665f0d2234777caff18fa12107f50
Abdulla <abdullaaxrolxonov@gmail.com>
2021-12-15 11:12:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+
+const persone = `Alisa`;
+console.log(persone);
+
+const bool = true;
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.isMarried);
+
+let arr = ['plum.png', 'orange.jpeg', 7, 'apple.bmp', {}, [],];
+console.log(arr[2]);
+
+// alert("Hello world");
+
+// const result = confirm(" Are you here?");
+// console.log(result);
+
+// const answer = prompt("Vam yest 18", "18");
+// console.log(answer + "da");
+
+// const answers = [];
+
+// answers[0] = prompt("kak vasha imya?", "");
+// answers[1] = prompt("kak vasha familya?", "");
+// answers[2] = prompt("Skolka vam let?", "");
+
+// console.log(typeof(answers));
+
+                // interpolyatsiya--------------
+// const category = 'toys';
+
+// console.log(`https://sqb.uz/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Salom, ${user}`);
+
+
+console.log('arr' + " - object");
+console.log(5 + "20");
