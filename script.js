d28ccdfaa914657b3b2957de4cb2182a3ada34a6
Abdulla070 <abdullaaxrolxonov@gmail.com>
2022-02-23 17:08:41 +0500

    2 amaliyot

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
-let numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
 
-let personalNewsDB = {
+const personalNewsDB = {
   count: numberOfNews,
   news: {},
   actors: {},
@@ -9,7 +9,27 @@ let personalNewsDB = {
   privat: false,
 };
 
-let qusion1 = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
-  qusion2 = +prompt("Unga qancha baho bergan bo'lar edingiz?");
-personalNewsDB.news[qusion1] = qusion2;
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
+    b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalNewsDB.news[a] = b;
+    console.log("tayyor");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
+
+if (personalNewsDB.count < 10) {
+  console.log("Juda oz sonli yangiliklar o'qilibdi");
+} else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
+  console.log("Siz klassik tomoshabinsiz");
+} else if (personalNewsDB.count >= 30) {
+  console.log("Siz yangilklar ishqibozisiz");
+} else {
+  console.log("Xato yuz berdi");
+}
+
 console.log(personalNewsDB);
