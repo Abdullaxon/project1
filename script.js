4326e2687d0b52153ef27a74fbe0d4a6cf1f76ae
Abdulla070 <abdullaaxrolxonov@gmail.com>
2022-02-28 12:38:29 +0500

    amaliy mashgulot

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
-const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
-
+let numberOfNews;
+  
+function startProject() {
+    numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+    while(numberOfNews == "" || numberOfNews == null || isNaN(numberOfNews)){
+      numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+    }
+  }
+ startProject();
+ 
+ 
 const personalNewsDB = {
   count: numberOfNews,
   news: {},
@@ -9,27 +18,45 @@ const personalNewsDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
-    b = prompt("Unga qancha baho bergan bo'lar edingiz?");
-
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalNewsDB.news[a] = b;
-    console.log("tayyor");
-  } else {
-    console.log("Error");
-    i--;
+function rememberNews() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
+      b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+  
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalNewsDB.news[a] = b;
+      console.log("tayyor");
+    } else {
+      console.log("Error");
+      i--;
+    }
   }
 }
+rememberNews();
 
-if (personalNewsDB.count < 10) {
-  console.log("Juda oz sonli yangiliklar o'qilibdi");
-} else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
-  console.log("Siz klassik tomoshabinsiz");
-} else if (personalNewsDB.count >= 30) {
-  console.log("Siz yangilklar ishqibozisiz");
-} else {
-  console.log("Xato yuz berdi");
+function showPersonalLevel(){
+  if (personalNewsDB.count < 10) {
+    console.log("Juda oz sonli yangiliklar o'qilibdi");
+  } else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
+    console.log("Siz klassik tomoshabinsiz");
+  } else if (personalNewsDB.count >= 30) {
+    console.log("Siz yangilklar ishqibozisiz");
+  } else {
+    console.log("Xato yuz berdi");
+  }
 }
+showPersonalLevel();
 
-console.log(personalNewsDB);
+function showMyDB(hidden){
+  if(!hidden){
+    console.log(personalNewsDB);
+  }
+}
+showMyDB(personalNewsDB.privat);
+
+function writeYourGenres() {
+  for(let i = 1; i <= 3; i++){
+    personalNewsDB.genres[i - 1] = prompt(`Sizning sevimli janringiz nomeri ${i}`);
+  }
+}
+writeYourGenres();
