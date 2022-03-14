809f82e071519e9ab85256ccd4b9d06e3856df44
Abdulla <abdullaaxrolxonov@gmail.com>
2022-03-14 11:29:28 +0500

    27 dars amaliyot

--- js/script.js
+++ js/script.js
@@ -16,14 +16,20 @@ Yangiliklarni massiv yordamida chiqaring. Html dan ochirip Faqat JS code yozip.
 */
 
 "use strict";
- 
-const newsGenre = document.querySelector(".promo__genre");
 
- //newsGenre.innerHTML = "UzNews"; //Dinamichiskiy kontentni o'zgartirish 1 usuli
+// const newsGenre = document.querySelector(".promo__genre");
+
+//newsGenre.innerHTML = "UzNews"; //Dinamichiskiy kontentni o'zgartirish 1 usuli
 
 // newsGenre.textContent = "UzNews"; //textContentga html tag yozib bomidi faqat InnerHtmlga yozsa boladi qavsni ichida <p>UzNews</p>
 
-newsGenre.insertAdjacentHTML("afterbegin","<p>Hello BBS world</p>");
+// newsGenre.insertAdjacentHTML("afterbegin","<p>Hello BBS world</p>");
+
+const btnNews = document.querySelector(".btn-news"),
+  newsGenre = document.querySelector(".promo__genre"),
+  bg = document.querySelector(".promo__bg"),
+  listNews = document.querySelector(".promo__interactive-list");
+
 const news = [
   "FOOTBALL",
   "BASKETBALL",
@@ -31,3 +37,19 @@ const news = [
   "BOX",
   "AMERICAN FOOTBAL IN EU...",
 ];
+
+btnNews.remove();
+newsGenre.textContent = "UzNews";
+newsGenre.style.color = "crimson";
+
+bg.style.backgroundImage = "url(img/2.jpg)";
+
+listNews.innerHTML = "";
+news.forEach((itemNews, index) => {
+  listNews.innerHTML += `
+    <li class="promo__interactive-item">
+      ${index + 1} ${itemNews}
+      <div class="delete"></div>
+    </li>
+    `;
+});
